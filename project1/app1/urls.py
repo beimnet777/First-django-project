2124a267a763e25cf93ccd6a97a9c8c94203d291
beimnet777 <91521577+beimnet777@users.noreply.github.com>
2022-02-21 00:38:53 -0800

    working on post methods and block content

--- project1/app1/urls.py
+++ project1/app1/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 
 from . import views
 
-urlpatterns=[path('',views.home,name="home"),]
+urlpatterns=[path('',views.home,name="home"),
+path('add/num',views.add),]
--- project1/app1/views.py
+++ project1/app1/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-  return HttpResponse("<h1>HELLO WORLD</h1>")
+  
+  return render(request,'home.html',{'name':"BEIMNET  "})
+  # return HttpResponse("<h1>HELLO WORLD</h1>")
+def add(request):
+  var1=request.POST['one']
+  var2=request.POST['two']
+  res=int(var1)+int(var2)
+  return render(request,"result.html",{'result':res})
